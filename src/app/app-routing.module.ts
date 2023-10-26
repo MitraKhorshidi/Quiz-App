413f7ac09fe02a1b1dc3368d17a0ba8a610123bc
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-10-26 17:14:20 +0200

    add getAllQuestions to dataService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { QuizComponent } from './pages/quiz/quiz.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: '/quiz', component: QuizComponent },
+  { path: 'quiz', component: QuizComponent },
 ];
 
 @NgModule({
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import questions from 'src/app/services/data';
+import { Question } from '../model/questions';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   constructor() { }
+
+  getAllQuestions():Observable<Array<Question>>{
+    return new Observable((observer)=>observer.next(questions));
+  }
 }
