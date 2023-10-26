1505b63a105496e0da1c1248ccc930663fcee97b
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-10-26 16:52:57 +0200

    add static database file

---
+++ src/app/services/data.js
@@ -0,0 +1,169 @@
+export default [
+    {
+        number : 1,
+        question : 'What does HTML stand for?',
+        options :[
+            {
+                label: 'Hyperlinks and Text Markup Language',
+            },
+            {
+                label: 'Home Tool Markup Language',
+            },
+            {
+                label: 'Hyper Text Markup Language',
+                answer:true,
+            },
+        ],
+    },
+    {
+        number : 2,
+        question : 'Choose the correct HTML element for the largest heading:',
+        options :[
+            {
+                label: '<Heading>',
+            },
+            {
+                label: '<Head>',
+            },
+            {
+                label: '<h6>',
+            },
+            {
+                label: '<h1>',
+                answer:true
+            },
+        ],
+    },
+    {
+        number : 3,
+        question : 'What is the correct HTML element for inserting a line break?',
+        options :[
+            {
+                label: '<break>',
+            },
+            {
+                label: '<br>',
+                answer:true
+            },
+            {
+                label: '<lb>',
+            },
+        ],
+    },
+    {
+        number : 4,
+        question : 'What is the correct HTML for adding a background color?',
+        options :[
+            {
+                label: '<background>yellow<background>',
+            },
+            {
+                label: '<div style="background-color:yellow;>',
+                answer:true
+            },
+            {
+                label: '<body bg="yellow">',
+            },
+        ],
+    },
+    {
+        number : 5,
+        question : 'Choose the correct HTML element to define important text',
+        options :[
+            {
+                label: '<important>',
+            },
+            {
+                label: '<strong',
+                answer:true
+            },
+            {
+                label: '<i>',
+            },
+        ],
+    },
+    {
+        number : 6,
+        question : 'Which character is used to indicate an end tag?',
+        options :[
+            {
+                label: '/',
+                answer:true
+            },
+            {
+                label: '>',
+            },
+            {
+                label: '*',
+            },
+            {
+                label: '<',
+            },
+        ],
+    },
+    {
+        number : 7,
+        question : 'How can you open a link in a new tab/browser window?',
+        options :[
+            {
+                label: '<a href="url" target="new"',
+            },
+            {
+                label: '<a href="url" target="_balnk"',
+            },
+            {
+                label: '<a href="url" new',
+                answer:true
+            },
+        ],
+    },
+    {
+        number : 8,
+        question : 'How can you make a numbered list?',
+        options :[
+            {
+                label: '<ul>',
+            },
+            {
+                label: '<li>',
+            },
+            {
+                label: '<ol>',
+                answer:true
+            },
+        ],
+    },
+    {
+        number : 9,
+        question : 'What is the correct HTML for making a checkbox?',
+        options :[
+            {
+                label: '<checkbox>',
+            },
+            {
+                label: '<input type="checkbox"/>',
+                answer:true
+            },
+            {
+                label: '<input type="check"/>',
+            },
+        ],
+    },
+    {
+        number : 10,
+        question : 'What is the correct HTML element for playing video files?',
+        options :[
+            {
+                label: '<movie>',
+            },
+            {
+                label: '<media>',
+            },
+            {
+                label: '<video>',
+                answer:true
+            },
+        ],
+    },
+
+]
