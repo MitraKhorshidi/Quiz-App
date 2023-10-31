a3b97051c32503845933e9f2ff5e6512b7d61f2e
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-10-31 13:05:12 +0100

    add post and getResut function

--- src/app/services/result.service.ts
+++ src/app/services/result.service.ts
@@ -1,12 +1,40 @@
 import { Injectable } from '@angular/core';
 
+
+const CORRECT_ANSWERS = [
+  'Hyper Text Markup Language',
+  '<h1>',
+  '<br>',
+  '<div style="background-color:yellow;>',
+  '<strong',
+  '/',
+  '<a href="url" new',
+  '<ol>',
+  '<input type="checkbox"/>',
+  '<video>'
+]
 @Injectable({
   providedIn: 'root'
 })
 export class ResultService {
 
+  answers: Array<string> = [];
+
   constructor() { }
 
-  post(answers:Array<string>){}
+  post(answers: Array<string>) {
+    this.answers = answers;
+
+  }
+
+  getresult():number{
+    let correctsNum: number = 0;
+    for (let i = 0; i < this.answers.length; i++) {
+      if (this.answers[i] === CORRECT_ANSWERS[i]) {
+        correctsNum += 1;
+      }
+    }
+    return(correctsNum);
+  }
 
 }
