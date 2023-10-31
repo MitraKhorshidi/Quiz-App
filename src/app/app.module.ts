79303aae358ff359b8d518cf5311cc2b7d8705c8
Mitra Khorshidi <mitkhorshidi@gmail.com>
2023-10-31 15:24:29 +0100

    fix onSubmit and submit input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -20,7 +20,7 @@ import { ResultComponent } from './pages/result/result.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
